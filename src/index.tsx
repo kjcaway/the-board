e5aff4cc7f1eaa67bf54707bd3b0a015d5b035e6
kjcaway <whdckstngh@naver.com>
2019-11-16 13:18:33 +0900

    web font

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './index.css';
 
 import { Provider } from "react-redux";
 import store from "./store/configureStore";
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: 'CookieRun Bold',
+  },
+});
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <MuiThemeProvider theme={theme}>
+      <App />
+    </MuiThemeProvider>
   </Provider>, document.getElementById('root'));
